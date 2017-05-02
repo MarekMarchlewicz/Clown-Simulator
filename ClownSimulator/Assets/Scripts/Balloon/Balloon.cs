5a90592a58af78b1a1845c5ba91ed3d7c33af4b2
Marek <marek.marchlewicz@gmail.com>
2017-05-02 19:29:46 +0800

    First tests with inflating and piercing balloons

---
+++ ClownSimulator/Assets/Scripts/Balloon/Balloon.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Balloon : MonoBehaviour
+{
+    [SerializeField]
+    private BalloonLifecycle lifecycle;
+
+    [SerializeField]
+    private GameObject balloonPointPrefab;
+
+    [SerializeField]
+    private float newBalloonPointRatio = 0.2f;
+
+    [SerializeField]
+    private float inflatingTime = 0.2f;
+
+    [SerializeField]
+    private float deflatingTime = 0.2f;
+
+    [SerializeField]
+    private float deflatingDelay = 0.05f;
+
+    public float ForceMultiplier = 5f;
+
+    private float lastSpawnBalloonPointTime;
+
+    private BalloonPoint lastAddedBallonPoint;
+
+    private Rigidbody mRigidBody;
+
+    private void Awake()
+    {
+        mRigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void AddForceAtPosition(Vector3 force, Vector3 position)
+    {
+        mRigidBody.AddForceAtPosition(force, position);
+    }
+
+    private void Update()
+    {
+        if (lifecycle == BalloonLifecycle.Inflating)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Time.time - lastSpawnBalloonPointTime > newBalloonPointRatio)
+                {
+                    Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+                    spawnPosition.z = 0f;
+
+                    BalloonPoint newBalloonPoint = Instantiate(balloonPointPrefab, spawnPosition, Quaternion.identity, transform).GetComponent<BalloonPoint>();
+
+                    newBalloonPoint.Initialize(this, inflatingTime);
+
+                    if (lastAddedBallonPoint != null)
+                    {
+                        lastAddedBallonPoint.AddNeighbour(newBalloonPoint);
+                        newBalloonPoint.AddNeighbour(lastAddedBallonPoint);
+                    }
+
+                    lastAddedBallonPoint = newBalloonPoint;
+
+                    lastSpawnBalloonPointTime = Time.time;
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                lifecycle = BalloonLifecycle.Idle;
+            }
+        }
+        else if (lifecycle == BalloonLifecycle.Idle)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit, 100f))
+                {
+                    BalloonPoint balloonPoint = hit.collider.GetComponent<BalloonPoint>();
+
+                    if(balloonPoint != null)
+                    {
+                        balloonPoint.Pierce(hit.point, deflatingTime, deflatingDelay);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ClownSimulator/Assets/Scripts/Balloon/BalloonPoint.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalloonPoint : MonoBehaviour
+{
+    private Balloon parent;
+
+    private List<BalloonPoint> neighbours = new List<BalloonPoint>();
+
+    public void Initialize(Balloon balloon, float inflatingTime)
+    {
+        parent = balloon;
+
+        transform.localScale = Vector3.zero;
+
+        StartCoroutine(Inflate(inflatingTime));
+    }
+
+    private IEnumerator Inflate(float inflatingTime)
+    {
+        float startTime = Time.time;
+
+        while(Time.time - startTime < inflatingTime)
+        {
+            float lerp = (Time.time - startTime) / inflatingTime;
+
+            transform.localScale = Vector3.one * lerp;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.localScale = Vector3.one;
+    }
+
+    public void Pierce(Vector3 piercePosition, float deflatingTime, float deflatingDelay)
+    {
+        StopAllCoroutines();
+
+        StartCoroutine(Deflate(piercePosition, deflatingTime, deflatingDelay));
+    }
+
+    private IEnumerator Deflate(Vector3 piercePosition, float deflatingTime, float deflatingDelay)
+    {
+        float startTime = Time.time;
+
+        while (Time.time - startTime < deflatingTime)
+        {
+            if(Time.time - startTime > deflatingDelay)
+            {
+                foreach(BalloonPoint neighbour in neighbours)
+                {
+                    neighbour.Pierce(transform.position, deflatingTime, deflatingDelay);
+                }
+            }
+
+            float lerp = (Time.time - startTime) / deflatingTime;
+
+            Vector3 force = (transform.position - piercePosition).normalized * (1f - lerp) * parent.ForceMultiplier;
+
+            parent.AddForceAtPosition(force, transform.position);
+
+            transform.localScale = Vector3.one * (1f - lerp);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        foreach (BalloonPoint neighbour in neighbours)
+        {
+            neighbour.RemoveNeighbour(this);
+        }
+
+        Destroy(gameObject);
+    }
+
+    public void AddNeighbour(BalloonPoint newNeighbour)
+    {
+        neighbours.Add(newNeighbour);
+    }
+
+    public void RemoveNeighbour(BalloonPoint neighbourToRemove)
+    {
+        neighbours.Remove(neighbourToRemove);
+    }
+
+    public List<BalloonPoint> GetNeighbours()
+    {
+        return neighbours;
+    }
+}
