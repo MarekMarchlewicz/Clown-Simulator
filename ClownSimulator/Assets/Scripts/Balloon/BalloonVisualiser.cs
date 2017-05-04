ba5066889a9844175704ab9445dab4af3876f1e2
Marek Marchlewicz <marek.marchlewicz@gmail.com>
2017-05-04 17:00:01 +0800

    very ugly solution - tests

--- ClownSimulator/Assets/Scripts/Balloon/BalloonVisualiser.cs
+++ ClownSimulator/Assets/Scripts/Balloon/BalloonVisualiser.cs
@@ -17,6 +17,8 @@ public class BalloonVisualiser : MonoBehaviour
 	private List<Vector3> vertices = new List<Vector3>();
 	private List<int> triangles = new List<int>();
 
+	[SerializeField] private float radius2 = 2f;
+
 	private int points;
 
 	public void Initialize(Balloon parent)
@@ -31,18 +33,20 @@ public class BalloonVisualiser : MonoBehaviour
 
 	private void Update()
 	{
-		vertices.Clear ();
-		triangles.Clear ();
+//		vertices.Clear ();
+//		triangles.Clear ();
+//
+//		points = ballon.GetPoints ().Count;
+//
+//		for (int i = 0; i < points; i++) 
+//		{
+//			AddTubeToPoint(i, ballon.GetPoints()[i]);    
+//		}
+//
+//		mesh.vertices = vertices.ToArray();
+//		mesh.triangles = triangles.ToArray();
 
-		points = ballon.GetPoints ().Count;
-
-		for (int i = 0; i < points; i++) 
-		{
-			AddTubeToPoint(i, ballon.GetPoints()[i]);    
-		}
-
-		mesh.vertices = vertices.ToArray();
-		mesh.triangles = triangles.ToArray();
+		Torus ();
 	}
 
 	private void AddTubeToPoint(int num, BalloonPoint point) 
@@ -51,29 +55,28 @@ public class BalloonVisualiser : MonoBehaviour
 
 		float turn = Mathf.PI * 2 / points;
 
-		//for (int s = 0; s < 2; s++) 
-		//{
-//			float angle = s * turn;
-//
-//			Vector3 vertex = point.transform.position + Quaternion.Euler (point.transform.right * angle) * point.transform.forward;
-//
-//			vertices.Add (vertex);
-		//}
-
-		vertices.Add (pointPosition - point.transform.up * point.transform.localScale.x * 0.5f);
-
-		vertices.Add (pointPosition + point.transform.up * point.transform.localScale.x * 0.5f);
-
-		if (vertices.Count >= 4) 
+		for (int s = 0; s < sides; s++) 
 		{
-			int start = vertices.Count - 4;
+			float angle = s * turn;
 
-			triangles.Add (start + 0);
-			triangles.Add (start + 1);
-			triangles.Add (start + 2);
-			triangles.Add (start + 1);
-			triangles.Add (start + 3);
-			triangles.Add (start + 2);    
+			Vector3 vertex = point.transform.position + Quaternion.Euler (point.transform.right * angle) * point.transform.forward;
+
+			vertices.Add (vertex);
+		}
+
+		if (vertices.Count >= sides * 4)
+		{
+			for (int side = 0; side < sides; sides++) 
+			{
+				int start = vertices.Count - sides * 4;
+
+				triangles.Add (start + 0);
+				triangles.Add (start + 1);
+				triangles.Add (start + 2);
+				triangles.Add (start + 1);
+				triangles.Add (start + 3);
+				triangles.Add (start + 2);
+			}
 		}
 	}
 
@@ -91,5 +94,84 @@ public class BalloonVisualiser : MonoBehaviour
 		p += ttt * p3;
 
 		return p;
+	}
+
+	private void Torus()
+	{
+		
+
+		List<BalloonPoint> points = ballon.GetPoints ();
+
+		int pointsCount = points.Count;
+
+		if (pointsCount < 2)
+			return;
+
+		#region Vertices		
+		Vector3[] vertices = new Vector3[(pointsCount+1) * (sides+1)];
+		float _2pi = Mathf.PI * 2f;
+		for( int seg = 0; seg <= pointsCount; seg++ )
+		{
+			
+			int currSeg = seg % (pointsCount);
+
+			float radius1 = points[currSeg].transform.localScale.x;
+
+			float t1 = (float)currSeg / pointsCount * _2pi;
+			Vector3 r1 = points[currSeg].transform.position;// new Vector3( Mathf.Cos(t1) * radius1, 0f, Mathf.Sin(t1) * radius1 );//
+
+			for( int side = 0; side <= sides; side++ )
+			{
+				int currSide = side == sides ? 0 : side;
+
+				Vector3 normale =  Vector3.Cross( r1, Vector3.up );
+				float t2 = (float)currSide / sides * _2pi;
+				Vector3 r2 = Quaternion.AngleAxis( -t1 * Mathf.Rad2Deg, Vector3.up ) *new Vector3( Mathf.Sin(t2) * radius2, Mathf.Cos(t2) * radius2 );
+
+				float angle = t2;
+
+				Vector3 vertex = Quaternion.Euler (points[currSeg].transform.right * t2 * Mathf.Rad2Deg) * points[currSeg].transform.up;
+
+				Debug.DrawRay(r1, vertex, Color.red);
+
+				r2 = vertex * radius2;
+
+				vertices[side + seg * (sides+1)] = r1 + r2;
+			}
+		}
+		#endregion
+
+		#region Triangles
+		int nbFaces = vertices.Length;
+		int nbTriangles = nbFaces * 2;
+		int nbIndexes = nbTriangles * 3;
+		int[] triangles = new int[ nbIndexes ];
+
+		int i = 0;
+		for( int seg = 0; seg <= pointsCount; seg++ )
+		{			
+			for( int side = 0; side <= sides - 1; side++ )
+			{
+				int current = side + seg * (sides+1);
+				int next = side + (seg < (pointsCount) ?(seg+1) * (sides+1) : 0);
+
+				if( i < triangles.Length - 6 )
+				{
+					triangles[i++] = current;
+					triangles[i++] = next;
+					triangles[i++] = next+1;
+
+					triangles[i++] = current;
+					triangles[i++] = next+1;
+					triangles[i++] = current+1;
+				}
+			}
+		}
+		#endregion
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+		mesh.RecalculateBounds ();
 	}
 }
